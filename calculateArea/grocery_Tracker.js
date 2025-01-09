dfacdeca368b6576ef105162871ae1b1377fc5e1
gitdb <dburk@maverickdean.com>
2025-01-09 10:21:30 -0500

    grocery calc example

---
+++ calculateArea/grocery_Tracker.js
@@ -0,0 +1,14 @@
+let groceries1;
+let groceries2;
+let groceries3;
+
+function calculateGroceries()() {
+    groceries1 = parseFloat(document.getElementById('groceries1').value);
+    groceries2 = parseFloat(document.getElementById('groceries2').value);
+    groceries3 = parseFloat(document.getElementById('groceries2').value);
+
+    let totalGroceries = groceries1 + groceries2 + groceries3;
+
+    document.getElementById('result').innerText = `The total amount is: ${totalGroceries}`;
+    
+}
