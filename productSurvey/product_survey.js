5eab4d6de7c683de016516e61a461cca2761f1b7
gitdb <dburk@maverickdean.com>
2025-07-28 11:06:29 -0400

    productSurvey v2

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -1,5 +1,13 @@
 function submitFeedback() {
+    const username = document.getElementById('name').value;
+    const age = document.getElementById('age').value;
+    const email = document.getElementById('email').value;
+    const job = document.getElementById('job').value;
+    const designation = document.getElementById('designation').value;
+    const productType = document.getElementById('productType').value;
+    const feedback = document.getElementById('feedbackText').value;
     alert('Thank you for your valuable feedback');
+    
     document.getElementById('userName').innerHTML = username;
     document.getElementById('userAge').innerHTML = age;
     document.getElementById('userEmail').innerHTML = email;
@@ -10,14 +18,6 @@ function submitFeedback() {
 
     document.getElementById('userInfo').style.display = 'block';
 }
-
-const username = document.getElementById('name').value;
-const age = document.getElementById('age').value;
-const email = document.getElementById('email').value;
-const job = document.getElementById('job').value;
-const designation = document.getElementById('designation').value;
-const productType = document.getElementById('productType').value;
-const feedback = document.getElementById('feedbackText').value;
 
 const submitButton=document.getElementById('submitBtn');
 
